0e65d47022e10a8725d42a8e81fa66285dedcfe0
Manik Maity <manikmaity156@gmail.com>
2024-01-03 22:12:14 +0530

    Completd prohect1 upto 60th

--- Project_01/script.js
+++ Project_01/script.js
@@ -5,6 +5,7 @@ let currentWeapon = 0;
 let fighting;
 let monsterHealth, inventory = ["stick"];
 
+
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
@@ -16,15 +17,42 @@ const goldText = document.querySelector("#goldText");
 const monsterName = document.querySelector("#monsterName");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterHealthText = document.querySelector("#monsterHealth");
+const locations = [
+    {
+        name: "town square",
+        "button text" : ["Go to store", "Go to cave", "Fight dragon"],
+        "button functions" : [goStore, goCave, fightDragon],
+        "text" : "You are in the town square. You see a sign that says \"Store\"."
+    }, 
+
+    {
+        name : "store",
+        "button text" : ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+        "button functions" : [buyHealth, buyWeapon, goTown],
+        "text" : "You enter the store."
+    }
+];
+
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+function update (location){
+    text.innerText = "You are in the town square. You see a sign that says \"Store\".";
+    button1.innerText = "Go to store";
+    button2.innerText = "Go to cave";
+    button3.innerText = "Fight dragon";
+    button1.onclick = goStore;
+    button2.onclick = goCave;
+    button3.onclick = fightDragon;
+}
+}
+
+function goTown(){
+    
 
 function goStore() {
-    text.innerText = "You enter the store.";
-    button1.innerText = "Buy 10 health (10 gold)";
-    button2.innerText = "Buy weapon (30 gold)";
-    button3.innerText = "Go to town square";
-    button1.onclick = buyHealth;
-    button2.onclick = buyWeapon;
-    button3.onclick = goTown;
+
 }
 function goCave(){
     console.log("Going to cave.");
@@ -33,16 +61,12 @@ function fightDragon (){
     console.log("Fighting dragon");
 }
 
-button1.onclick = goStore;
-button2.onclick = goCave;
-button3.onclick = fightDragon;
-
 function buyHealth(){
 
 }
 function buyWeapon(){
 
 }
-function goTown(){
-    
-}
+
+
+
