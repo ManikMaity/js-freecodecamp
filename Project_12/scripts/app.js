53f2022543a4860d8034df06efde47da01ed463b
Manik Maity <manikmaity156@gmail.com>
2024-04-12 21:58:52 +0530

    Updated project12 expense tracker with functionality

--- Project_12/scripts/app.js
+++ Project_12/scripts/app.js
@@ -4,13 +4,13 @@ import localStorage from "./localStorage.js";
 
 const colors = {
   red: "#F38181",
-  green: "#95E1D3",
+  green: "#297054b0",
   yellow: "#FCE38A",
   purple: "#8b8dff",
   lightBlue: "#d2dfff",
 };
 
-const totalBudget = localStorage.getTotalBudget();
+let totalExpData, totalBudgetLeftData;
 
 // ---------------------------------refrense of html element here---------------------------
 const ctx = document.getElementById("myChart");
@@ -20,7 +20,8 @@ const totalExpEle = document.getElementById("totalExp");
 const addExpBtnEle = document.querySelector(".add-exp-btn");
 const addBudBtnEle = document.querySelector(".add-bud-btn");
 const expForSelectEle = document.querySelector(".exp-for");
-const allOptionLabel = document.querySelectorAll(".exp-for label");
+const tagContainer = document.querySelector(".tags-conatiner");
+let allOptionLabel = document.querySelectorAll(".tags-conatiner label");
 const addBtnEle = document.getElementById("addBtn");
 const clearBtnEle = document.getElementById("clearBtn");
 const transAmountEle = document.getElementById("addAmount");
@@ -28,19 +29,61 @@ const expForEle = document.querySelectorAll('[name="expFor"]');
 const transHistoryParentEle = document.querySelector(".money-history-container");
 const mobileAddScreenShowBtn = document.querySelector(".mobile-add-btn");
 const moneyAddCardEle = document.querySelector(".add-money-card");
+const addNewTagBtnEle = document.getElementById("addTagBtn");
+const confirmTagBtnEle = document.getElementById("confirmNewTag");
+const tagInputEle = document.querySelector(".tag-input");
+const tagInputField = document.getElementById("tagInputField");
 
 // -----------------------------code logic here --------------------------------
 
-totalBudgetEle.textContent = totalBudget;
+
+function totalCalculate() {
+  const allTrans = localStorage.getAllTrans();
+  let total = 0;
+  for (let i = 0; i < allTrans.length; i++) {
+    total += allTrans[i].amount;
+  }
+  totalExpEle.textContent = `${total}`;
+  const leftBudget = Number(localStorage.getTotalBudget()) - total;
+  totalExpData = total;
+  totalBudgetLeftData = leftBudget;
+  budgetLeftEle.textContent = `${leftBudget}`
+  totalBudgetEle.textContent = localStorage.getTotalBudget();
+}
+
+totalCalculate();
+
+function addBudgetInput() {
+  if (transAmountEle.value == "") {
+    console.log("Please enter your budget.");
+  } else {
+    localStorage.setTotalBudget(Number(transAmountEle.value));
+    totalCalculate();
+  }
+  transAmountEle.value = "";
+}
+
 
 const showBudgetInput = () => {
   addExpBtnEle.classList.remove("selected-add-exp");
   addBudBtnEle.classList.add("selected-add-bud");
   expForSelectEle.style.display = "none";
+  transAmountEle.value = localStorage.getTotalBudget();
+  addBtnEle.removeEventListener("click", addTransItem);
+  addBtnEle.addEventListener("click", addBudgetInput);
+};
+
+const showExpInput = () => {
+  addBudBtnEle.classList.remove("selected-add-bud");
+  addExpBtnEle.classList.add("selected-add-exp");
+  expForSelectEle.style.display = "flex";
+  transAmountEle.value = "";
+  addBtnEle.removeEventListener("click", addBudgetInput);
+  addBtnEle.addEventListener("click", addTransItem);
 };
 
 
-function createTranHTML (obj = {}){
+function createTranHTML(obj = {}) {
   return `<div class="trans-item" id="${obj?.id}">
   <div>
       <h4>-₹${obj?.amount}</h4>
@@ -55,43 +98,84 @@ function createTranHTML (obj = {}){
       <button id="transDelete"><i class="fa-regular fa-trash-can"></i></button>
   </div>
   </div>`
+} {
+  /* <input type="radio" id="subscription" name="expFor" value="Subscription📱">
+  <label for="subscription">Subscription📱</label> */
+}
+
+localStorage.saveTag("Manik");
+
+function createTagHTML(str) {
+  return `
+  <input type="radio" id="${str}" name="expFor" value="${str}">
+  <label for="${str}">${str}</label>
+  `
+}
+
+function renderTags() {
+  tagContainer.innerHTML = ``;
+  const tagArray = localStorage.getAllTags();
+  if (tagArray == []) {
+    return;
+  } else {
+    tagArray.forEach(tag => {
+      const tagEle = createTagHTML(tag);
+      tagContainer.insertAdjacentHTML("afterbegin", tagEle);
+    })
+  }
+  allOptionLabel = document.querySelectorAll(".exp-for label");
+  allOptionLabel.forEach((label) => {
+    label.addEventListener("click", () => {
+      allOptionLabel.forEach((label) => {
+        label.style.backgroundColor = colors.lightBlue;
+      });
+      label.style.backgroundColor = colors.yellow;
+    });
+  });
+}
+
+renderTags();
+
+function addNewTag() {
+  const tagValue = tagInputField.value;
+  if (tagValue == "") {
+    console.log("Plz input tag name");
+  } else {
+    localStorage.saveTag(tagValue);
+    renderTags();
+  }
+  tagInputField.value = "";
+  tagInputEle.classList.remove("show");
 }
 
 
-function renderTransHistory (){
+function renderTransHistory() {
   transHistoryParentEle.innerHTML = "";
   const transArr = localStorage.getAllTrans();
-  if (transArr == []){
+  if (transArr == []) {
     return;
-  }
-  else{
+  } else {
     transArr.forEach(transObj => {
       const transEle = createTranHTML(transObj);
       transHistoryParentEle.insertAdjacentHTML('beforeend', transEle);
     })
   }
-  
+
 }
+
 renderTransHistory();
 
-const showExpInput = () => {
-  addBudBtnEle.classList.remove("selected-add-bud");
-  addExpBtnEle.classList.add("selected-add-exp");
-  expForSelectEle.style.display = "flex";
-};
 
 function showChart(arr = []) {
   new Chart(ctx, {
     type: "pie",
     data: {
-      labels: ["Expence", "Buget Left", "Income"],
-      datasets: [
-        {
-          data: arr,
-          backgroundColor: [colors.red, colors.purple, colors.green],
-          borderWidth: 0,
-        },
-      ],
+      labels: ["Expence", "Buget Left"],
+      datasets: [{
+        data: arr,
+        backgroundColor: [colors.red, colors.green],
+        borderWidth: 0,
+      }, ],
     },
     options: {
       plugins: {
@@ -122,7 +206,7 @@ function addTransItem() {
   const amount = amountEle.value;
   const checkedTagValue = checkedTag ? checkedTag.value : undefined;
 
-  if (amount && checkedTagValue) {
+  if (amount && checkedTagValue && Number(amount) > 0) {
     let transObj = {
       id: Math.floor(Math.random() * 10000000),
       amount: Number(amount),
@@ -131,9 +215,10 @@ function addTransItem() {
     };
     localStorage.saveTrans(transObj);
     renderTransHistory();
-  } 
-  
-  else {
+    addTranBtnEvent();
+    totalCalculate();
+
+  } else {
     if (amount == "") {
       console.log("Enter the ammount");
     } else if (checkedTagValue == undefined) {
@@ -147,24 +232,38 @@ function addTransItem() {
   checkedLabel.style.backgroundColor = colors.lightBlue;
 }
 
+function addTranBtnEvent() {
+  document.querySelectorAll(".trans-item").forEach(item => {
+    item.lastElementChild.lastElementChild.addEventListener("click", () => {
+      console.log(item.id);
+      const sure = window.confirm("Are you really wanna delete this?");
+      if (sure) {
+        localStorage.deleteTrans(item.id);
+        renderTransHistory();
+        addTranBtnEvent();
+        totalCalculate();
+      }
+
+    })
+  })
+}
 
 
-allOptionLabel.forEach((label) => {
-  label.addEventListener("click", () => {
-    allOptionLabel.forEach((label) => {
-      label.style.backgroundColor = colors.lightBlue;
-    });
-    label.style.backgroundColor = colors.yellow;
-  });
-});
-
-mobileAddScreenShowBtn.addEventListener("click", (e)=>{
+mobileAddScreenShowBtn.addEventListener("click", (e) => {
   moneyAddCardEle.classList.toggle("show");
   mobileAddScreenShowBtn.children[0].classList.toggle("rotatePlus");
 
 })
+
 addBudBtnEle.addEventListener("click", showBudgetInput);
 addExpBtnEle.addEventListener("click", showExpInput);
 addBtnEle.addEventListener("click", addTransItem);
+addNewTagBtnEle.addEventListener("click", () => {
+  tagInputEle.classList.toggle("show");
+});
+confirmTagBtnEle.addEventListener("click", addNewTag);
 
-showChart([50, 30, 20]);
+
+
+addTranBtnEvent();
+showChart([totalExpData, totalBudgetLeftData]);
--- Project_12/scripts/localStorage.js
+++ Project_12/scripts/localStorage.js
@@ -19,7 +19,7 @@ export default class display {
         const allTrans = display.getAllTrans();
         const exiting = allTrans.find(item => item.id == trans.id);
         if (exiting){
-
+            
         }
         else{
             allTrans.unshift(trans);
@@ -27,4 +27,30 @@ export default class display {
 
         localStorage.setItem("trans-data", JSON.stringify(allTrans));
     }
+
+    static deleteTrans (id){
+        const allTrans = display.getAllTrans();
+        const tranId = Number(id);
+        const filteredTrans = allTrans.filter(item => item.id != tranId);
+        localStorage.setItem("trans-data", JSON.stringify(filteredTrans));
+        
+    }
+
+    static getAllTags (){
+        const allTags  = localStorage.getItem("tags") || "[]";
+        return JSON.parse(allTags);
+    }
+
+    static saveTag (str){
+        const allTags = display.getAllTags();
+        const exiting = allTags.find(tag => tag == str);
+        if (exiting){
+            return `Tag already exit`;
+        }
+        else{
+            allTags.push(str);
+        }
+        localStorage.setItem("tags", JSON.stringify(allTags));
+    }
+    
 }
