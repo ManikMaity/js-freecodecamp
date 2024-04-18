5c9fff3e3256b560cf48cefafdc2ae6347aeb6d9
Manik Maity <manikmaity156@gmail.com>
2024-04-18 09:30:17 +0530

    Used web scraping to get cricket data

--- Others/WebScraping/index.js
+++ Others/WebScraping/index.js
@@ -1,5 +1,19 @@
 const fs = require('fs');
 const puppeteer = require('puppeteer');
+
+let cricketData = {};
+
+/*{
+        name: "Héctor Zelada",
+        position: "goalkeeper",
+        number: 22,
+        isCaptain: false,
+        nickname: null,
+      }
+       */
+
+
+
 let webImages = [];
 
 const getPicture = async (name, num) => {
@@ -16,9 +30,35 @@ const getPicture = async (name, num) => {
     await browser.close();
 }
 
-getPicture("ai man foodball image", 22).then(()=>{
+getPicture("ai cricket images", 50).then(()=>{
     fs.writeFileSync("./image.json", JSON.stringify(webImages));
 })
+
+
+// const getCricketData = async () => {
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     await page.goto("https://www.bcci.tv/players");
+//     await page.waitForSelector('[onclick="click_player(this)"]');
+//     const players = await page.evaluate(() => Array.from(document.querySelectorAll('[onclick="click_player(this)"]'), (e) => {
+//         const playerStats = e.querySelectorAll(".player-type p");
+//         let tempObj = {
+//             name : e.querySelector(".player-name").textContent,
+//             number : Number(e.querySelector(".t-shirt").textContent),
+//             playerType : playerStats[0].textContent,
+//             age : Number(playerStats[1].textContent),
+//             hand : playerStats[2].textContent
+
+//         };
+//         return tempObj;
+
+//     }));
+//     cricketData.players = players;
+//     fs.writeFileSync("./indian-cricket-players-data.json", JSON.stringify(cricketData, undefined, 5));
+   
+//     await browser.close();
+// }
+// getCricketData();
 
 
 
